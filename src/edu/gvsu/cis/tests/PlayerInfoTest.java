2eba6c475e291f911dda95e758c673e44ca5d125
Matrix159 <joseldridge15@gmail.com>
2016-04-02 13:47:08 -0400

    Writing PlayerInfo test cases

--- src/edu/gvsu/cis/tests/PlayerInfoTest.java
+++ src/edu/gvsu/cis/tests/PlayerInfoTest.java
@@ -1,7 +1,11 @@
 package edu.gvsu.cis.tests;
 
 import edu.gvsu.cis.SmiteMaster;
+import edu.gvsu.cis.playerinfo.PlayerInfo;
 import org.junit.Test;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 import static org.junit.Assert.assertTrue;
 
@@ -10,9 +14,27 @@ public class PlayerInfoTest {
 
 
     @Test
-    public void testPlayerStrings() throws Exception{
+    public void testPlayerStrings() throws Exception
+    {
         SmiteMaster master = new SmiteMaster();
-        assertTrue("Created_Datetime", master.getPlayer("Matrix159").get(0).getCreated_Datetime() != null);
+        List<PlayerInfo> list = master.getPlayer("Matrix159");
+        for(PlayerInfo x : list)
+        {
+            assertTrue("Avatar_URL", x.getAvatar_URL()!= null);
+            assertTrue("Created_Datetime", x.getCreated_Datetime() != null);
+            assertTrue("Last_Login_Datetime", x.getLast_Login_Datetime() != null);
+            assertTrue("Name", x.getName() != null);
+            assertTrue("RankedConquest", x.getRankedConquest() != null);
+            assertTrue("RankedDuel", x.getRankedDuel() != null);
+            assertTrue("RankedJoust", x.getRankedJoust() != null);
+            assertTrue("Team_Name", x.getTeam_Name() != null);
+            assertTrue("ret_msg", x.getRet_msg() == null);
+        }
+    }
+
+    @Test
+    public void testPlayerInts() throws Exception
+    {
 
     }
 }
